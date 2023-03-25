a6d81b7eebd26833616ff2476224c69d55db7d8b
Justin Kindrix <jkindrix@gmail.com>
2023-03-25 02:02:37 -0500

    Add initial DbContext class

---
+++ ProgressTracker.Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using ProgressTracker.Domain;
+
+namespace ProgressTracker.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Domain.Task> Tasks { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+    }
+}
