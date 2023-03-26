f6ea21fdb9cb7a5853c46ae4b36d5734d4221daf
Justin Kindrix <jkindrix@gmail.com>
2023-03-26 00:10:06 -0500

    Add object extensions class

---
+++ ProgressTracker.Common/Extensions/ObjectExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace ProgressTracker.Common
+{
+    public static class ObjectExtensions
+    {
+        public static bool TrySetProperty(this object obj, string property, object value)
+        {
+            var prop = obj.GetType().GetProperty(property, BindingFlags.Public | BindingFlags.Instance);
+            if (prop != null && prop.CanWrite)
+            {
+                prop.SetValue(obj, value, null);
+                return true;
+            }
+            return false;
+        }
+    }
+}
